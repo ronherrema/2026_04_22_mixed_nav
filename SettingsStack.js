afc4f5f4d5698e3c24809009147f01b499e5f134
Ron Herrema <rherrema@gmail.com>
2026-04-23 11:58:25 +0100

    restyle header background

--- SettingsStack.js
+++ SettingsStack.js
@@ -2,15 +2,20 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import ThemesScreen from "./Themes"
 import ProfileScreen from "./Profile"
 import SettingsScreen from "./Settings"
+import { useReState } from "@raulpesilva/re-state"
 
 const Stack = createNativeStackNavigator()
 
 export default function SettingsStackScreen() {
+  const [bgc, setBgc] = useReState("bgc")
   return (
     <Stack.Navigator
       screenOptions={{
         headerTitle: "",
         headerShadowVisible: false,
+        headerStyle: {
+          backgroundColor: bgc,
+        },
       }}
     >
       <Stack.Screen name="Settings" component={SettingsScreen} />
