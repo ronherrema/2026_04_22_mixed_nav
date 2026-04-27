7650115f8ca075e4b1c7c27f6f57400866a59be5
Ron Herrema <rherrema@gmail.com>
2026-04-27 23:04:14 +0100

    not sure

--- Themes.js
+++ Themes.js
@@ -1,17 +1,20 @@
-import { SafeAreaView } from "react-native-safe-area-context"
-import { Button, StyleSheet, Text } from "react-native"
+import { Button, StyleSheet, Text, View } from "react-native"
 import { useStore } from "./Store"
 
 export default function ThemesScreen() {
   const { bgc, setBgc } = useStore()
 
   return (
-    <SafeAreaView style={[styles.container, { backgroundColor: bgc }]}>
-      <Text style={{ fontSize: 36 }}>Themes</Text>
+    <View
+      style={[styles.container, { backgroundColor: bgc }]}
+      edges={["left", "right", "bottom"]}
+    >
+      <Text style={{ fontSize: 36, color: "#fFF" }}>Themes</Text>
       <Button title="Green" onPress={() => setBgc("#77ff77ff")}></Button>
-      <Button title="White" onPress={() => setBgc("#fff")}></Button>
       <Button title="Purple" onPress={() => setBgc("#7777ff77")}></Button>
-    </SafeAreaView>
+      <Button title="Salmon" onPress={() => setBgc("#ff777777")}></Button>
+      <Button title="White" onPress={() => setBgc("#fff")}></Button>
+    </View>
   )
 }
 
@@ -19,7 +22,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
     backgroundColor: "white",
   },
 })
