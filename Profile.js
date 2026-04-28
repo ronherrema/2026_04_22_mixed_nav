d44dca9353b62d01f52f5fcd6975cb6f909a431b
Ron Herrema <rherrema@gmail.com>
2026-04-28 10:41:55 +0100

    first commit

--- Profile.js
+++ Profile.js
@@ -1,12 +1,27 @@
-import { Text, StyleSheet, View } from "react-native"
+import { Text, StyleSheet, View, TextInput } from "react-native"
 import { useStore } from "./Store"
+import { useState } from "react"
 
 export default function ProfileScreen() {
-  const { bgc } = useStore()
+  const { bgc, profile, setProfile } = useStore()
+  const [input, setInput] = useState("")
 
   return (
     <View style={[styles.container, { backgroundColor: bgc }]}>
       <Text style={{ fontSize: 36, color: "#fff" }}>Profile</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter name"
+        value={input}
+        onChangeText={(t) => setInput(t)}
+        onSubmitEditing={() => {
+          setProfile({ name: input })
+          // setInput("")
+        }}
+        clearTextOnFocus={true} // iOS
+        clearButtonMode="always" // iOS
+      ></TextInput>
+      <Text style={styles.name}>Name: {profile.name}</Text>
     </View>
   )
 }
@@ -17,5 +32,18 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start",
     backgroundColor: "white",
+  },
+  input: {
+    width: 300,
+    height: 40,
+    padding: 5,
+    borderWidth: 1,
+    alignSelf: "flex-start",
+    margin: 20,
+  },
+  name: {
+    alignSelf: "flex-start",
+    margin: 20,
+    fontSize: 20,
   },
 })
--- Store.js
+++ Store.js
@@ -5,12 +5,23 @@ import AsyncStorage from "@react-native-async-storage/async-storage"
 export const useStore = create(
   persist(
     (set) => ({
+      // single key-value pair
       bgc: "white",
       setBgc: (color) => set({ bgc: color }),
 
+      // an array
       selectedItems: [],
       addSelectedItem: (item) =>
         set((state) => ({ selectedItems: [...state.selectedItems, item] })),
+
+      // a JS object
+      profile: {
+        name: "",
+        email: "",
+        address: "",
+      },
+      setProfile: (data) =>
+        set((state) => ({ profile: { ...state.profile, ...data } })),
     }),
     {
       name: "storage",
