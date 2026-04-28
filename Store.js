7098d9f9ba1ab761ae7d707d1d4c77c011d7aaf7
Ron Herrema <rherrema@gmail.com>
2026-04-28 09:59:57 +0100

    delete unused

--- Store.js
+++ Store.js
@@ -2,11 +2,6 @@ import { create } from "zustand"
 import { persist, createJSONStorage } from "zustand/middleware"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 
-// export const useStore = create((set) => ({
-//   bgc: "white",
-//   setBgc: (color) => set({ bgc: color }),
-// }))
-
 export const useStore = create(
   persist(
     (set) => ({
